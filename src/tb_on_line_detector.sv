d9b86e70fdccef02851fb05081c21a596d527679
FangameEmpire <fangameempire@gmail.com>
2025-06-27 00:08:45 -0700

    Distance-To-Line submodule testbenches 50% done

--- src/tb_on_line_detector.sv
+++ src/tb_on_line_detector.sv
@@ -28,9 +28,9 @@ endmodule // on_line_detector_testbench
 module between_testbench();
 	// Choose a width
 	localparam WIDTH = 5;
-	MIN_WIDTH_INT = 0;				MIN_WIDTH = MIN_WIDTH_INT[WIDTH - 1:0];
-	MID_WIDTH_INT = 2**(WIDTH - 1);	MID_WIDTH = MID_WIDTH_INT[WIDTH - 1:0];
-	MAX_WIDTH_INT = 2**(WIDTH);		MAX_WIDTH = MAX_WIDTH_INT[WIDTH - 1:0];
+	localparam MIN_WIDTH_INT = 0;					localparam MIN_WIDTH = MIN_WIDTH_INT[WIDTH - 1:0];
+	localparam MID_WIDTH_INT = 2**(WIDTH - 1) - 1;	localparam MID_WIDTH = MID_WIDTH_INT[WIDTH - 1:0];
+	localparam MAX_WIDTH_INT = 2**(WIDTH) - 1;		localparam MAX_WIDTH = MAX_WIDTH_INT[WIDTH - 1:0];
 	
 	// Module ports
 	logic [WIDTH - 1:0] in_ax, in_ay, in_bx, in_by, in_cx, in_cy;
@@ -48,12 +48,95 @@ module between_testbench();
 	initial begin
 		// BOUNDS
 		current_test = "BOUNDARY LINES";
+		{in_ax, in_ay, in_bx, in_by, in_cx, in_cy} = {6{MIN_WIDTH}};	#DELAY;
+		{in_cx, in_cy} = {2{MAX_WIDTH}};								#DELAY;
+		{in_ax, in_ay} = {2{MID_WIDTH}};								#DELAY;
+		{in_cx, in_cy} = {2{MIN_WIDTH}};								#DELAY;
+		{in_ax, in_ay, in_bx, in_by, in_cx, in_cy} = {6{MAX_WIDTH}};	#DELAY;
+		
+		// RANDOM INPUTS
+		current_test = "RANDOM INPUTS";
+		for (int i = 0; i < RUNS_RANDOM; i++) begin
+			in_ax = $random; in_ay = $random; in_bx = $random; in_by = $random; in_cx = $random; in_cy = $random; #DELAY;
+		end // for i
 		
 	end // initial
 	
 endmodule // between_testbench
 
-// steep_checker
+module steep_checker_testbench();
+	// Choose a width
+	localparam WIDTH = 5;
+	localparam MIN_WIDTH_INT = 0;					localparam MIN_WIDTH = MIN_WIDTH_INT[WIDTH - 1:0];
+	localparam MID_WIDTH_INT = 2**(WIDTH - 1) - 1;	localparam MID_WIDTH = MID_WIDTH_INT[WIDTH - 1:0];
+	localparam MAX_WIDTH_INT = 2**(WIDTH) - 1;		localparam MAX_WIDTH = MAX_WIDTH_INT[WIDTH - 1:0];
+	
+	// Module ports
+	logic [WIDTH - 1:0] in_x0, in_y0, in_x1, in_y1;
+	logic out_steep;
+	
+	// Device under test
+	steep_checker #(.WIDTH(WIDTH)) dut (.*);
+	
+	// Simulation parameters
+	localparam RUNS_RANDOM = 20;
+	localparam DELAY = 1;
+	string current_test;
+	
+	// Module tests
+	initial begin
+		// Slopes = 0, 1, INF
+		current_test = "SLOPES 0, 1, INF";
+		
+		// Corners
+		{in_x0, in_y0, in_x1, in_y1} = {4{MIN_WIDTH}};	#DELAY;
+		{in_x1, in_y1} = {2{MAX_WIDTH}};				#DELAY;
+		in_x0 = MAX_WIDTH; in_x1 = MIN_WIDTH;			#DELAY;
+		in_y0 = MAX_WIDTH;								#DELAY;
+		in_x0 = MIN_WIDTH; in_x1 = MAX_WIDTH;			#DELAY;
+		in_x0 = MAX_WIDTH; in_y0 = MIN_WIDTH;			#DELAY;
+		{in_x0, in_x1} = {2{MIN_WIDTH}};				#DELAY;
+		
+		// Centers
+		current_test = "CENTERS, SLOPE = 1";
+		in_x0 = MID_WIDTH; in_y0 = MIN_WIDTH; in_x1 = MAX_WIDTH; in_y1 = MID_WIDTH; #DELAY;
+		in_y0 = MAX_WIDTH;															#DELAY;
+		in_x1 = MIN_WIDTH;															#DELAY;
+		in_y0 = MIN_WIDTH;															#DELAY;
+		
+		// Slopes = 0.5, 2
+		current_test = "SLOPES 0.5, 2";
+		
+		// Center-left fixed
+		in_x0 = MIN_WIDTH; in_y0 = MID_WIDTH; in_x1 = MAX_WIDTH; in_y1 = MIN_WIDTH; #DELAY;
+		in_y1 = MID_WIDTH;															#DELAY;
+		in_y1 = MAX_WIDTH;															#DELAY;
+		
+		// Center-top fixed
+		in_x0 = MID_WIDTH; in_y0 = MIN_WIDTH; in_x1 = MIN_WIDTH; in_y1 = MAX_WIDTH; #DELAY;
+		in_x1 = MID_WIDTH;															#DELAY;
+		in_x1 = MAX_WIDTH;															#DELAY;
+		
+		// Center-right fixed
+		in_x0 = MAX_WIDTH; in_y0 = MID_WIDTH; in_x1 = MIN_WIDTH; in_y1 = MIN_WIDTH; #DELAY;
+		in_y1 = MID_WIDTH;															#DELAY;
+		in_y1 = MAX_WIDTH;															#DELAY;
+		
+		// Bottom-right fixed
+		in_x0 = MID_WIDTH; in_y0 = MAX_WIDTH; in_x1 = MIN_WIDTH; in_y1 = MIN_WIDTH; #DELAY;
+		in_x1 = MID_WIDTH;															#DELAY;
+		in_x1 = MAX_WIDTH;															#DELAY;
+		
+		// Random inputs
+		current_test = "RANDOM INPUTS";
+		for (int i = 0; i < RUNS_RANDOM; i++) begin
+			in_x0 = $random; in_y0 = $random; in_x1 = $random; in_y1 = $random; #DELAY;
+		end // for i
+		
+	end // initial
+	
+endmodule // steep_checker_testbench
+
 // mismatch
 // cross_product_length
 
